eb7d5b013a3dfc0d124733bc1cbfe749e3b23989
kycnexynpuwejl <kycnexynpuwejl@protonmail.com>
2022-05-25 15:59:46 +0300

    rework data annotations

--- Scaffold/Model/Location.cs
+++ Scaffold/Model/Location.cs
@@ -1,11 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace Scaffold.Model
 {
+    [Table("location")]
+    [Index("Id", Name = "location_id_uindex", IsUnique = true)]
     public partial class Location
     {
-        public int? Id { get; set; }
-        public string? City { get; set; }
+        public Location()
+        {
+            Storages = new HashSet<Storage>();
+        }
+
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("city")]
+        [StringLength(50)]
+        [Unicode(false)]
+        public string City { get; set; } = null!;
+
+        [InverseProperty("Location")]
+        public virtual ICollection<Storage> Storages { get; set; }
     }
 }
--- Scaffold/Model/User.cs
+++ Scaffold/Model/User.cs
@@ -1,15 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace Scaffold.Model
 {
+    [Table("user")]
+    [Index("Email", Name = "user_email_uindex", IsUnique = true)]
+    [Index("Id", Name = "user_id_uindex", IsUnique = true)]
+    [Index("Mobile", Name = "user_mobile_uindex", IsUnique = true)]
     public partial class User
     {
-        public int? Id { get; set; }
+        public User()
+        {
+            Carts = new HashSet<Cart>();
+        }
+
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("firstname")]
+        [StringLength(50)]
+        [Unicode(false)]
         public string? Firstname { get; set; }
+        [Column("lastname")]
+        [StringLength(50)]
+        [Unicode(false)]
         public string? Lastname { get; set; }
+        [Column("email")]
+        [StringLength(50)]
+        [Unicode(false)]
         public string? Email { get; set; }
+        [Column("password_hash")]
+        [StringLength(32)]
+        [Unicode(false)]
         public string? PasswordHash { get; set; }
+        [Column("mobile")]
+        [StringLength(15)]
+        [Unicode(false)]
         public string? Mobile { get; set; }
+
+        [InverseProperty("User")]
+        public virtual ICollection<Cart> Carts { get; set; }
     }
 }
